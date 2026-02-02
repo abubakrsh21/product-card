e0a4add266bedb1748fcfde98d94cdb5137fa910
Abubakr Shakirov <musayid@outlook.com>
2026-02-02 03:30:57 +0200

    homework7

---
+++ comments.js
@@ -0,0 +1,74 @@
+// (5), (6) Добавление файла и создание экспортируемую переменную.
+
+export const comments = [
+  {
+    postId: 1,
+    id: 1,
+    name: "id labore ex et quam laborum",
+    email: "Eliseo@gardner.biz",
+    body: "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+  },
+  {
+    postId: 1,
+    id: 2,
+    name: "quo vero reiciendis velit similique earum",
+    email: "Jayne_Kuhic@sydney.com",
+    body: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
+  },
+  {
+    postId: 1,
+    id: 3,
+    name: "odio adipisci rerum aut animi",
+    email: "Nikita@garfield.biz",
+    body: "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"
+  },
+  {
+    postId: 1,
+    id: 4,
+    name: "alias odio sit",
+    email: "Lew@alysha.tv",
+    body: "non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati"
+  },
+  {
+    postId: 1,
+    id: 5,
+    name: "vero eaque aliquid doloribus et culpa",
+    email: "Hayden@althea.biz",
+    body: "harum non quasi et ratione\nut voluptate esse quasi\nnihil sed et\nvoluptates impedit sunt"
+  },
+  {
+    postId: 1,
+    id: 6,
+    name: "et fugit eligendi deleniti quidem qui sint nihil autem",
+    email: "Presley.Mueller@myrl.com",
+    body: "doloribus at sed quis culpa deserunt consectetur qui praesentium\naccusamus fugiat dicta\nmolestiae rerum eaque et facilis sed repudiandae\nveniam quaerat nulla et accusamus nisi facilis"
+  },
+  {
+    postId: 1,
+    id: 7,
+    name: "repellat consequatur praesentium vel minus molestias voluptatum",
+    email: "Dallas@ole.me",
+    body: "maiores sed dolores similique labore et inventore et\nquasi temporibus esse sunt id et\nlaborum voluptates vitae quia qui et\nvel quisquam unde et veniam reprehenderit"
+  },
+  {
+    postId: 1,
+    id: 8,
+    name: "et omnis dolorem",
+    email: "Mallory_Kunze@marie.org",
+    body: "ut voluptatem corrupti velit\nad voluptatem maiores\net nisi velit vero accusamus maiores\nvoluptates quia aliquid ullam eaque"
+  },
+  {
+    postId: 1,
+    id: 9,
+    name: "provident id voluptas",
+    email: "Meghan_Littel@rene.us",
+    body: "similique fugit est\nillum et dolorum ab et ea voluptas\net suscipit ratione quia doloribus praesentium\naut voluptate distinctio qui"
+  },
+  {
+    postId: 1,
+    id: 10,
+    name: "eaque et deleniti atque tenetur ut quo ut",
+    email: "Carmen_Keeling@caroline.name",
+    body: "voluptate iusto quis nobis reprehenderit ipsum amet nulla\nquia quas dolores velit et non\nex t [...]"
+  }
+];
