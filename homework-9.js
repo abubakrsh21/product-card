aa207aff801ecda2c827c52bc649a7c4744adb49
Abubakr Shakirov <musayid@outlook.com>
2026-02-26 14:41:28 +0200

    fixed errors

--- homework-9.js
+++ homework-9.js
@@ -33,20 +33,19 @@ const closeBtn = document.getElementById('modal-close');
 const modal = document.getElementById('modal');
 const overlay = document.getElementById('overlay');
 
-openBtn.addEventListener('click', () => {
+function openModal() {
     modal.classList.add('modal-showed');
     overlay.classList.add('modal-showed');
-});
+}
 
-closeBtn.addEventListener('click', () => {
+function closeModal() {
     modal.classList.remove('modal-showed');
     overlay.classList.remove('modal-showed');
-});
+}
 
-overlay.addEventListener('click', () => {
-    modal.classList.remove('modal-showed');
-    overlay.classList.remove('modal-showed');
-});
+openBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
 
 // (6) Создание формы для модального окна (регистрации)
@@ -58,9 +57,8 @@ let user = null;
 registerForm.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    if (!registerForm.checkValidity()) {
-        alert('Форма заполнена неверно');
-        registerForm.reportValidity();
+    if (!form.checkValidity()) {
+        form.reportValidity();
         return;
     }
 
@@ -72,14 +70,21 @@ registerForm.addEventListener('submit', function (event) {
         return;
     }
 
+    const firstName = registerForm.firstName.value.trim();
+    const lastName = registerForm.lastName.value.trim();
+    const birthDate = registerForm.birthDate.value;
+    const login = registerForm.login.value.trim();
+    const password2 = registerForm.password.value.trim();
+    const createdOn = new Date();
+
     user = {
-        firstName: registerForm.firstName.value.trim(),
-        lastName: registerForm.lastName.value.trim(),
-        birthDate: registerForm.birthDate.value,
-        login: registerForm.login.value.trim(),
-        password: password,
-        createdOn: new Date()
-    }
+        firstName,
+        lastName,
+        birthDate,
+        login,
+        password2,
+        createdOn
+    };
 
     console.log(user);
 
